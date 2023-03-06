ad9f6f8d4dc40f28a0d9e0ab6d09df97a31b87e6
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2023-03-06 17:26:41 -0600

    basic backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,14 @@ import express from "express";
 
 type Config = {
   port: number;
+  host: string;
+  httpProtocol: "http" | "https";
 };
 
 const config: Config = {
   port: Number(process.env.PORT) || 8080,
+  host: process.env.HOST || "localhost",
+  httpProtocol: (process.env.HTTP_PROTOCOL as "https" | "http") || "http",
 };
 
 const main = () => {
@@ -16,14 +20,18 @@ const main = () => {
   });
 
   app.use("/users", (_req, res) => {
-    res.status(200).json({ users: [{ name: "ben" }] });
+    res.status(200).json({ users: [{ name: "test user" }] });
   });
 
   app.use("/", (_req, res) => {
     res.status(404).json({ message: "not found" });
   });
 
-  app.listen(config.port);
+  app.listen(config.port, config.host, () => {
+    console.log(
+      `server listening at ${config.httpProtocol}://${config.host}:${config.port}`
+    );
+  });
 };
 
 main();
